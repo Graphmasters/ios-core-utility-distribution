d12a535706ec96ffa1b3cb59cb24f2d2a6db2378
gm-machine <username@users.noreply.github.com>
2026-06-23 09:18:07 +0000

    :bookmark: 1.2.12 [ci skip]

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GMCoreUtility",
-            url: "https://www.github.com/Graphmasters/ios-core-utility-distribution/releases/download/1.2.11/GMCoreUtility.xcframework.zip",
-            checksum: "e602379a0eceb7ad45901995a9695f224fca42cd21a1e52691e42a1939cb925a"
+            url: "https://www.github.com/Graphmasters/ios-core-utility-distribution/releases/download/1.2.12/GMCoreUtility.xcframework.zip",
+            checksum: "35482ae3d572988e834b742e1272910834a494ca473b3808992a4db82d0cc9d5"
         )
     ]
 )
